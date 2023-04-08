3198d297503f45fafc17b1fc20504799e0dd0e81
Ivan Trj <ivantrajanovski@gmail.com>
2023-04-08 12:39:17 +0200

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Ivan Trajanovski on 08.04.23.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
